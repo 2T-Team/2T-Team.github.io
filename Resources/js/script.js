afac321751b8ddc1ba563eadd62beab563a17023
githubfrostpixel <73395529+githubfrostpixel@users.noreply.github.com>
2021-04-26 05:08:00 +0700

    header

--- Resources/js/script.js
+++ Resources/js/script.js
@@ -2,11 +2,7 @@ function hidText()
 {
     var hT=document.getElementById('headerText');
     if(hT.style.top=="40vh")
-    {
         hT.style.top="70vh";
-    }
     else
-    {
         hT.style.top="40vh";
-    }
 }
