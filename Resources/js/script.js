bd5a97ed6cf1ca16f013e6026d819af1a95ee474
githubfrostpixel <73395529+githubfrostpixel@users.noreply.github.com>
2021-04-26 02:54:31 +0700

    replace

---
+++ Resources/js/script.js
@@ -0,0 +1,12 @@
+function hidText()
+{
+    var hT=document.getElementById('headerText');
+    if(hT.style.top=="40vh")
+    {
+        hT.style.top="70vh";
+    }
+    else
+    {
+        hT.style.top="40vh";
+    }
+}
