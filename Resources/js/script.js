b42a93d48343ad684bced9a7c63c7a23a6f50fa6
githubfrostpixel <73395529+githubfrostpixel@users.noreply.github.com>
2021-04-26 19:28:41 +0700

    main

--- Resources/js/script.js
+++ Resources/js/script.js
@@ -6,3 +6,26 @@ function hidText()
     else
         hT.style.top="40vh";
 }
+
+window.onscroll = function() {mySticky()};
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+var logo= document.getElementById("logo");
+// Get the offset position of the navbar
+var sticky = 800;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function mySticky() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky");
+    navbar.classList.add("bg-bluet");
+    navbar.classList.remove("bg-transparent");
+    logo.style.height="50px";
+  } else {
+    navbar.classList.remove("sticky");
+    navbar.classList.add("bg-transparent");
+    navbar.classList.remove("bg-bluet");
+    logo.style.height="75px";
+  }
+}
